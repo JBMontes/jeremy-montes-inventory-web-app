6d7a3e4946b88b09a06a8b79b6f5a305bc639fc5
Jeremy Montes <jeremymontes@pursuit.org>
2023-06-27 10:48:56 -0400

    Created a functional update form

--- index.js
+++ index.js
@@ -12,3 +12,44 @@ for (let shirts of inventory) {
     });
   }
 }
+
+
+
+let form = document.querySelector("form")
+let collection = document.querySelector(".collection")
+let div = document.createElement("div")
+div.classList.add("shirt")
+let shirtImage = document.createElement("img")
+
+
+form.addEventListener("submit" , (e)=>{
+    e.preventDefault();
+
+    let paragraph = document.createElement("p")
+    let paragraph1 = document.createElement("p")
+    let paragraph2 = document.createElement("p")
+    let paragraph3 = document.createElement("p")
+    let paragraph4 = document.createElement("p")
+    let paragraph5 = document.createElement("p")
+    let button = document.createElement("button")
+
+    shirtImage.src = e.target["image"].value
+  
+   div.append(shirtImage)
+   paragraph1.innerText =`Name: ${e.target["name"].value}`
+   div.append(paragraph1)
+   paragraph2.innerText = `Price: $${e.target["price"].value}`
+   div.append(paragraph2)
+   paragraph3.innerText = `Size: ${e.target["size"].value}`
+   div.append(paragraph3)
+   paragraph4.innerText = `In-Stock: ${e.target["in-stock"].value}`
+   div.append(paragraph4)
+   paragraph5.innerText = `Condition: ${e.target["condition"].value}`
+   div.append(paragraph5)
+   button.innerText = "Remove"
+   div.append(button)
+
+   collection.append(div)
+   e.target.reset();
+
+})
