86fe092c0442868f1f0566f014efa0d6eb64ca93
Jeremy Montes <jeremymontes@pursuit.org>
2023-06-28 12:22:23 -0400

    created a in stock toggle button and made visual edits

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let removals = document.querySelectorAll("button");
+let removals = document.querySelectorAll("#removeButton");
 
 let form = document.querySelector("form");
 
@@ -7,6 +7,13 @@ let collection = document.querySelector(".collection");
 let div = document.createElement("div");
 
 let hOne = document.querySelector("h1")
+
+formUpdate = document.querySelector(".formUpdate")
+
+let inStock = document.querySelectorAll("#in-stock")
+
+
+
 
 
 div.classList.add("shirt");
@@ -22,14 +29,36 @@ form.addEventListener("submit", (e) => {
   let paragraph1 = document.createElement("p");
   let paragraph2 = document.createElement("p");
   let paragraph3 = document.createElement("p");
-  let paragraph4 = document.createElement("p");
+  let br = document.createElement("br");
+  let instockButton = document.createElement("button")
   let paragraph5 = document.createElement("p");
   let button = document.createElement("button");
+  button.setAttribute("id","removeButton")
+  
   button.addEventListener("click", (e) => {
     e.preventDefault();
     e.target.parentElement.remove();
     e.target.reset();
+
+   
   });
+
+    let clicks = 0
+    instockButton.addEventListener("click", (e)=>{
+         e.preventDefault();
+        clicks++
+         if(clicks % 2 ===1){
+            instockButton.textContent = "In Stock ✖︎";
+            instockButton.style.background = "red";
+            instockButton.style.textcolor = "white"
+         }
+         else{
+            instockButton.textContent = "In Stock ✔︎";
+            instockButton.style.background = "green";
+            instockButton.style.textcolor = "white";
+         }
+    })
+
 
   
   shirtImage.src = e.target["image"].value;
@@ -43,15 +72,21 @@ form.addEventListener("submit", (e) => {
   div.append(paragraph2);
   paragraph3.innerText = `Size: ${e.target["size"].value}`;
   div.append(paragraph3);
-  paragraph4.innerText = `In-Stock: ${e.target["in-stock"].value}`;
-  div.append(paragraph4);
   paragraph5.innerText = `Condition: ${e.target["condition"].value}`;
   div.append(paragraph5);
+  div.append(br)
+  instockButton.textContent = "In Stock ✔︎" 
+  instockButton.setAttribute("id", "in-stock")
+  div.append( instockButton)
+  div.append(br)
+ 
   button.innerText = "Remove";
   div.append(button);
   div.append(hr)
-
   collection.append(div);
+
+collection.style.background = "white"
+collection.style.border = "3px solid black"
 
   e.target.reset();
 });
@@ -63,19 +98,37 @@ for (let removalButton of removals) {
     e.preventDefault();
     e.target.parentElement.remove();
     
-    if(collection.childElementCount > 0){
-        collection.style.background = "white"
-        collection.style.border = "3px solid black"
-        
-    }
-    else{
+    if(collection.childElementCount < 1){
+
         collection.style.background = "none"
         collection.style.border = "none"
+        formUpdate.style.padding="300px"
   }
     });
 
   let resetButton = document.querySelector("#resetButton")
   resetButton.addEventListener("click", (e)=>{
-        e.target.reset()  // IS THIS THE KIND OF RESET THEY WANT TO SEE?
+        formUpdate.reset()  
   })
+}
+
+
+for(let stock of inStock){
+    let clicks = 0
+    stock.addEventListener("click", (e)=>{
+         e.preventDefault();
+        clicks++
+         if(clicks % 2 ===1){
+            stock.textContent = "In Stock ✖︎";
+            stock.style.background = "red";
+            stock.style.textcolor = "white"
+         }
+         else{
+            stock.textContent = "In Stock ✔︎";
+            stock.style.background = "green";
+            stock.style.textcolor = "white"
+         }
+
+      
+    })
 }
