04a5bb5ea6df13172be7d37b0ca4401145dc4e38
Jeremy Montes <jeremymontes@pursuit.org>
2023-06-29 11:09:38 -0400

    Made edits to css

--- index.js
+++ index.js
@@ -6,134 +6,158 @@ let collection = document.querySelector(".collection");
 
 let div = document.createElement("div");
 
-let hOne = document.querySelector("h1")
+let hOne = document.querySelector("h1");
 
-formUpdate = document.querySelector(".formUpdate")
+formUpdate = document.querySelector(".formUpdate");
 
-let inStock = document.querySelectorAll("#in-stock")
+let inStock = document.querySelectorAll("#in-stock");
 
-
-
-
+let errorMessage = document.querySelector(".errorMessage");
 
 div.classList.add("shirt");
-// let shirtImage = document.createElement("img");
 
 form.addEventListener("submit", (e) => {
+  console.log(e.target);
   e.preventDefault();
+
   let div = document.createElement("div");
   div.classList.add("shirt");
-  let hr = document.createElement("HR") 
+  let hr = document.createElement("HR");
   let shirtImage = document.createElement("img");
   let paragraph = document.createElement("p");
+  shirtImage.src = e.target["image"].value;
   let paragraph1 = document.createElement("p");
   let paragraph2 = document.createElement("p");
   let paragraph3 = document.createElement("p");
   let br = document.createElement("br");
-  let instockButton = document.createElement("button")
+  let instockButton = document.createElement("button");
   let paragraph5 = document.createElement("p");
+
   let button = document.createElement("button");
-  button.setAttribute("id","removeButton")
-  
+  button.setAttribute("id", "removeButton");
   button.addEventListener("click", (e) => {
     e.preventDefault();
     e.target.parentElement.remove();
-    e.target.reset();
-
-   
+    e.reset();
   });
 
-    let clicks = 0
-    instockButton.addEventListener("click", (e)=>{
-         e.preventDefault();
-        clicks++
-         if(clicks % 2 ===1){
-            instockButton.textContent = "In Stock ✖︎";
-            instockButton.style.background = "red";
-            instockButton.style.textcolor = "white"
-         }
-         else{
-            instockButton.textContent = "In Stock ✔︎";
-            instockButton.style.background = "green";
-            instockButton.style.textcolor = "white";
-         }
-    })
+  let clicks = 0;
+  instockButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    clicks++;
+    if (clicks % 2 === 1) {
+      instockButton.textContent = "In Stock ✖︎";
+      instockButton.style.background = "red";
+      instockButton.style.textcolor = "white";
+    } else {
+      instockButton.textContent = "In Stock ✔︎";
+      instockButton.style.background = "green";
+      instockButton.style.textcolor = "white";
+    }
+  });
 
+  //Start of Append Process
+  if(e.target["image"].value === ""){
+    errorMessage.removeAttribute("hidden");
+    errorMessage.innerText = "Image Field Required";
+    errorMessage.style.border = "3px solid black";
+    errorMessage.style.backgroundColor = "white";
 
+    e.target["errorMessage"].reset()
+
+  }else{
+    div.append(shirtImage);
+    errorMessage.innerText = "";
+     errorMessage.style.background = "none";
+     errorMessage.style.border = "0px";
+  }
   
-  shirtImage.src = e.target["image"].value;
-  div.append(shirtImage);
-
   div.append(hr);
 
   paragraph1.innerText = `Name: ${e.target["name"].value}`;
-  div.append(paragraph1);
+ 
+  if(e.target["name"].value === ""){
+    errorMessage.removeAttribute("hidden");
+    errorMessage.innerText = "Name Field Required";
+    errorMessage.style.border = "3px solid black";
+    errorMessage.style.backgroundColor = "white"; 
+
+    e.target["errorMessage"].reset();
+   
+  }else{
+
+     div.append(paragraph1);
+     errorMessage.innerText = "";
+     errorMessage.style.background = "none";
+     errorMessage.style.border = "0px";
+}
+
   paragraph2.innerText = `Price: $${e.target["price"].value}`;
+
   div.append(paragraph2);
   paragraph3.innerText = `Size: ${e.target["size"].value}`;
   div.append(paragraph3);
   paragraph5.innerText = `Condition: ${e.target["condition"].value}`;
   div.append(paragraph5);
-  div.append(br)
-  
-  instockButton.setAttribute("id", "in-stock")
-  instockButton.textContent = e.target["in-stock"].textContent 
-  instockButton.style.backgroundColor = e.target["in-stock"].style.backgroundColor
-  instockButton.style.textcolor = e.target["in-stock"].style.textcolor
-  
-  div.append(instockButton)
-  div.append(br)
- 
+  div.append(br);
+
+  instockButton.setAttribute("id", "in-stock");
+  instockButton.textContent = e.target["in-stock"].textContent;
+  instockButton.style.backgroundColor =
+    e.target["in-stock"].style.backgroundColor;
+  instockButton.style.textcolor = e.target["in-stock"].style.textcolor;
+
+  div.append(instockButton);
+  div.append(br);
+
   button.innerText = "Remove";
   div.append(button);
-  div.append(hr)
+  div.append(hr);
   collection.append(div);
 
-collection.style.background = "white"
-collection.style.border = "3px solid black"
+  collection.style.background = "white";
+  collection.style.border = "3px solid black";
+
 
   e.target.reset();
 });
 
-
+// collection remove button
 for (let removalButton of removals) {
   removalButton.addEventListener("click", (e) => {
-    console.log(e.target);
     e.preventDefault();
     e.target.parentElement.remove();
-    
-    if(collection.childElementCount <= 0){
 
-        collection.style.background = "none"
-        collection.style.border = "none"
-        formUpdate.style.padding="350px"
-  }
-    });
+    if (collection.childElementCount <= 0) {
+      collection.style.background = "none";
+      collection.style.border = "none";
+      formUpdate.style.padding = "350px";
+    }
+  });
 
-  let resetButton = document.querySelector("#resetButton")
-  resetButton.addEventListener("click", (e)=>{
-        formUpdate.reset()  
-  })
+  let resetButton = document.querySelector("#resetButton");
+  resetButton.addEventListener("click", (e) => {
+    formUpdate.reset();
+    errorMessage.innerText = "";
+    errorMessage.style.background = "none";
+    errorMessage.style.border = "0px";
+  });
 }
 
-
-for(let stock of inStock){
-    let clicks = 0
-    stock.addEventListener("click", (e)=>{
-         e.preventDefault();
-        clicks++
-         if(clicks % 2 ===1){
-            stock.textContent = "In Stock ✖︎";
-            stock.style.background = "red";
-            stock.style.textcolor = "white"
-         }
-         else{
-            stock.textContent = "In Stock ✔︎";
-            stock.style.background = "green";
-            stock.style.textcolor = "white"
-
-         }
-
-      
-    })
+//collection in stock button
+for (let stock of inStock) {
+  let clicks = 0;
+  stock.addEventListener("click", (e) => {
+    e.preventDefault();
+    clicks++;
+    if (clicks % 2 === 1) {
+      stock.textContent = "In Stock ✖︎";
+      stock.style.background = "red";
+      stock.style.textcolor = "white";
+    } else {
+      stock.textContent = "In Stock ✔︎";
+      stock.style.background = "green";
+      stock.style.textcolor = "white";
+    }
+  });
 }
